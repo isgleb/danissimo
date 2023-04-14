2438db9596dec54485a057eb24de0e9fe8afb390
isgleb <71262423+isgleb@users.noreply.github.com>
2023-04-14 19:17:17 +0300

    js

---
+++ main.js
@@ -0,0 +1,22 @@
+// const label = document.getElementsByClassName('label')[0];
+// console.log(label)
+
+
+
+
+
+
+window.onload=function(){
+
+    function keyDown(){
+        console.log("sadjfhglkasfghlkashfgk")
+        stickers.classList.add("hidden")
+    }
+
+    const label = document.getElementsByClassName('label')[0];
+    label?.addEventListener('click', keyDown);
+
+    const stickers = document.getElementById("stickers-wrapper")
+
+
+}

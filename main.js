bc9bb7d65ea1f2e50f6d90b4d68b4eb2f593176c
isgleb <71262423+isgleb@users.noreply.github.com>
2023-04-15 12:27:59 +0300

    sticker transitions

--- main.js
+++ main.js
@@ -1,18 +1,6 @@
 
 window.onload= async () => {
 
-    function keyDown() {
-        console.log("sadjfhglkasfghlkashfgk")
-        stickers.classList.add("hidden")
-        label.classList.add("wrapper-right-hidden")
-
-    }
-
-    const label = document.getElementsByClassName('label-wrapper')[0];
-    label?.addEventListener('click', keyDown);
-
-    // label.classList.remove("wrapper-hidden")
-
     const stickers = document.getElementById("stickers-wrapper")
 
     const leftTopSticker = document.getElementsByClassName("sticker top left")[0];
@@ -24,18 +12,6 @@ window.onload= async () => {
     const leftBottomSticker = document.getElementsByClassName("sticker bottom left")[0];
     const bottomCenterSticker = document.getElementsByClassName("sticker bottom-center x-center")[0];
 
-
-    console.log(
-        leftTopSticker,
-        topSticker,
-        topRightSticker,
-        leftCenterSticker,
-        rightCenterSticker,
-        rightBottomSticker,
-        leftBottomSticker,
-        bottomCenterSticker
-    )
-
     async function showLabel(delayTime) {
         new Promise(resolve => setTimeout(resolve, delayTime)).finally(() => {
             label?.classList.remove("wrapper-hidden");
@@ -43,4 +19,24 @@ window.onload= async () => {
     }
 
     await showLabel(500)
+
+
+    function showAd() {
+        console.log("sadjfhglkasfghlkashfgk")
+        // stickers.classList.add("hidden")
+        label.classList.add("wrapper-hidden")
+
+        leftTopSticker.classList.add("top-left-transition")
+        topSticker.classList.add("top-transition")
+        topRightSticker.classList.add("top-right-transition")
+        leftCenterSticker.classList.add("left-transition")
+        rightCenterSticker.classList.add("right-transition")
+        rightBottomSticker.classList.add("bottom-right-transition")
+        leftBottomSticker.classList.add("bottom-left-transition")
+        bottomCenterSticker.classList.add("bottom-transition")
+
+    }
+
+    const label = document.getElementsByClassName('label-wrapper')[0];
+    label?.addEventListener('click', showAd);
 }

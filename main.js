47a93e6ac4c3cddebec00ee2a33472b748e86cf8
isgleb <71262423+isgleb@users.noreply.github.com>
2023-04-17 16:10:20 +0300

    pointer

--- main.js
+++ main.js
@@ -23,17 +23,17 @@ window.onload= async () => {
         bottomCenterSticker
     ]
 
-    // async function showLabel(delayTime) {
-    //     new Promise(resolve => setTimeout(resolve, delayTime)).finally(() => {
-    //         label?.classList.remove("wrapper-hidden");
-    //     })
-    // }
+    async function showLabel(delayTime) {
+        new Promise(resolve => setTimeout(resolve, delayTime)).finally(() => {
+            label?.classList.remove("wrapper-hidden");
+        })
+    }
 
-    // await showLabel(400)
+    await showLabel(400)
 
 
     function showAd() {
-        // label.classList.add("wrapper-hidden")
+        label.classList.add("wrapper-hidden")
 
         // stickers.forEach((sticker) => {
         //     // const xDefault;
@@ -73,15 +73,16 @@ window.onload= async () => {
         // });
     }
 
+    const label = document.getElementsByClassName('label-wrapper')[0];
+    label?.addEventListener('click', showAd);
 
-    // const label = document.getElementsByClassName('label-wrapper')[0];
-    // label?.addEventListener('click', showAd);
-    // label.classList.add("wrapper-hidden")
 
     let isDragging = false
     let offset = [0,0];
 
-    topSticker?.addEventListener('mousedown', (event) => {
+    // console.log(topSticker)
+
+    topSticker?.addEventListener('pointerdown', (event) => {
         isDragging = true
         console.log(event)
         event.target.offsetLeft
@@ -90,10 +91,10 @@ window.onload= async () => {
             topSticker.offsetLeft - event.clientX,
             topSticker.offsetTop - event.clientY
         ];
-    }, false)
+    }, true)
 
-    document.addEventListener("mousemove", (event) => {
-        console.log(event)
+    document.addEventListener("touchmove", (event) => {
+
         event.preventDefault();
         if (isDragging) {
             console.log(event)
@@ -101,9 +102,8 @@ window.onload= async () => {
             topSticker.style.top  = (event.clientY + offset[1]) + 'px';
         }
     })
-    //
-    // document.addEventListener('touchend', function() {
-    //     console.log(event)
-    //     isDragging = false;
-    // }, true);
+
+    document.addEventListener('touchend', function() {
+        isDragging = false;
+    }, true);
 }

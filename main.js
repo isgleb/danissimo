4e64c940448983700b98fdb1e6206902872597e8
isgleb <71262423+isgleb@users.noreply.github.com>
2023-04-17 19:51:03 +0300

    blue box

--- main.js
+++ main.js
@@ -1,14 +1,28 @@
 
-window.onload= async () => {
 
-    const leftTopSticker = document.getElementsByClassName("sticker top left")[0];
-    let topSticker = document.getElementsByClassName("sticker top x-center")[0];
-    const topRightSticker = document.getElementsByClassName("sticker top right")[0];
-    const leftCenterSticker = document.getElementsByClassName("sticker y-center left")[0];
-    const rightCenterSticker = document.getElementsByClassName("sticker y-center right")[0];
-    const rightBottomSticker = document.getElementsByClassName("sticker bottom right")[0];
-    const leftBottomSticker = document.getElementsByClassName("sticker bottom left")[0];
-    const bottomCenterSticker = document.getElementsByClassName("sticker bottom-center x-center")[0];
+// $(document).ready( function() {
+
+    console.log("loaded")
+
+    var leftTopSticker = document.getElementsByClassName("sticker top left")[0];
+    var topSticker = document.getElementsByClassName("sticker top x-center")[0];
+    var topRightSticker = document.getElementsByClassName("sticker top right")[0];
+    var leftCenterSticker = document.getElementsByClassName("sticker y-center left")[0];
+    var rightCenterSticker = document.getElementsByClassName("sticker y-center right")[0];
+    var rightBottomSticker = document.getElementsByClassName("sticker bottom right")[0];
+    var leftBottomSticker = document.getElementsByClassName("sticker bottom left")[0];
+    var bottomCenterSticker = document.getElementsByClassName("sticker bottom-center x-center")[0];
+
+    // var leftTopSticker = $(".sticker.top.left")[0];
+    // var topSticker = $(".sticker.top.x-center")[0];
+    // var topRightSticker = $(".sticker.top.right")[0];
+    // var leftCenterSticker = $(".sticker.y-center.left")[0];
+    // var rightCenterSticker = $(".sticker.y-center.right")[0];
+    // var rightBottomSticker = $(".sticker.bottom.right")[0];
+    // var leftBottomSticker = $(".sticker.bottom.left")[0];
+    // var bottomCenterSticker = $(".sticker.bottom-center.x-center")[0];
+
+    console.log(leftTopSticker)
 
     const stickers = [
         { el: leftTopSticker, x: 0, y: 0, isDragging: false },
@@ -21,13 +35,13 @@ window.onload= async () => {
         { el: bottomCenterSticker, x: 0, y: 0, isDragging: false},
     ]
 
-    async function showLabel(delayTime) {
-        new Promise(resolve => setTimeout(resolve, delayTime)).finally(() => {
-            label?.classList.remove("wrapper-hidden");
-        })
-    }
-
-    await showLabel(400)
+    // async function showLabel(delayTime) {
+    //     new Promise(resolve => setTimeout(resolve, delayTime)).finally(() => {
+    //         label?.classList.remove("wrapper-hidden");
+    //     })
+    // }
+    //
+    // await showLabel(400)
 
     function showAd() {
         label.classList.add("wrapper-hidden")
@@ -41,12 +55,21 @@ window.onload= async () => {
 
 
     stickers?.forEach( sticker => {
-        sticker.el?.addEventListener('touchstart', (event) => {
+        sticker.el.addEventListener("touchstart", (event) => {
             console.log(event)
             sticker.x = sticker.el.offsetLeft - event.clientX
             sticker.y = sticker.el.offsetTop - event.clientY
             sticker.isDragging = true
         }, false)
+
+
+
+        // sticker.el?.addEventListener('touchstart', (event) => {
+        //     console.log(event)
+        //     sticker.x = sticker.el.offsetLeft - event.clientX
+        //     sticker.y = sticker.el.offsetTop - event.clientY
+        //     sticker.isDragging = true
+        // }, false)
     })
 
     document.addEventListener("touchmove", (event) => {
@@ -66,4 +89,4 @@ window.onload= async () => {
             sticker.isDragging = false
         })
     }, false);
-}
+// })

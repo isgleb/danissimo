c5f2b83e76646b6e2751be709a5fbe81df9af35f
isgleb <71262423+isgleb@users.noreply.github.com>
2023-04-17 23:25:02 +0300

    refact

--- main.js
+++ main.js
@@ -1,92 +1,47 @@
 
+const label = document.getElementById('swipe-label');
+label.addEventListener('click', showAd);
 
-// $(document).ready( function() {
+new Promise(resolve => setTimeout(resolve, 400))
+    .finally(() => {
+        label.classList.remove("hidden");
+})
 
-    console.log("loaded")
+function showAd() {
+    label.classList.add("hidden")
+    label.addEventListener("transitionend", () => {
+        label.style.display="none"
+    });
+}
 
-    var leftTopSticker = document.getElementsByClassName("sticker top left")[0];
-    var topSticker = document.getElementsByClassName("sticker top x-center")[0];
-    var topRightSticker = document.getElementsByClassName("sticker top right")[0];
-    var leftCenterSticker = document.getElementsByClassName("sticker y-center left")[0];
-    var rightCenterSticker = document.getElementsByClassName("sticker y-center right")[0];
-    var rightBottomSticker = document.getElementsByClassName("sticker bottom right")[0];
-    var leftBottomSticker = document.getElementsByClassName("sticker bottom left")[0];
-    var bottomCenterSticker = document.getElementsByClassName("sticker bottom-center x-center")[0];
+let activeBlock = null;
+let initialX = 0;
+let initialY = 0;
 
-    // var leftTopSticker = $(".sticker.top.left")[0];
-    // var topSticker = $(".sticker.top.x-center")[0];
-    // var topRightSticker = $(".sticker.top.right")[0];
-    // var leftCenterSticker = $(".sticker.y-center.left")[0];
-    // var rightCenterSticker = $(".sticker.y-center.right")[0];
-    // var rightBottomSticker = $(".sticker.bottom.right")[0];
-    // var leftBottomSticker = $(".sticker.bottom.left")[0];
-    // var bottomCenterSticker = $(".sticker.bottom-center.x-center")[0];
+const blocks = document.querySelectorAll('.sticker');
 
-    console.log(leftTopSticker)
+blocks.forEach(block => {
+    block.addEventListener('touchstart', dragStart);
+    block.addEventListener('touchend', dragEnd);
+    block.addEventListener('touchmove', drag);
+})
 
-    const stickers = [
-        { el: leftTopSticker, x: 0, y: 0, isDragging: false },
-        { el: topSticker, x: 0, y: 0, isDragging: false },
-        { el: topRightSticker, x: 0, y: 0, isDragging: false},
-        { el: leftCenterSticker, x: 0, y: 0, isDragging: false},
-        { el: rightCenterSticker, x: 0, y: 0, isDragging: false},
-        { el: rightBottomSticker, x: 0, y: 0, isDragging: false},
-        { el: leftBottomSticker, x: 0, y: 0, isDragging: false},
-        { el: bottomCenterSticker, x: 0, y: 0, isDragging: false},
-    ]
+function dragStart(e) {
+    initialX = e.touches[0].clientX - e.target.offsetLeft;
+    initialY = e.touches[0].clientY - e.target.offsetTop;
+    activeBlock = e.target;
+}
 
-    // async function showLabel(delayTime) {
-    //     new Promise(resolve => setTimeout(resolve, delayTime)).finally(() => {
-    //         label?.classList.remove("wrapper-hidden");
-    //     })
-    // }
-    //
-    // await showLabel(400)
+function dragEnd() {
+    initialX = 0;
+    initialY = 0;
+    activeBlock = null;
+}
 
-    function showAd() {
-        label.classList.add("wrapper-hidden")
-        label.addEventListener("transitionend", ()=>{
-            label.style.display="none"
-        });
+function drag(e) {
+    if (activeBlock) {
+        e.preventDefault();
+        e.target.style.left = `${ e.touches[0].clientX - initialX }px`;
+        e.target.style.top = `${ e.touches[0].clientY - initialY }px`;
     }
-
-    const label = document.getElementsByClassName('label-wrapper')[0];
-    label?.addEventListener('click', showAd);
-
-
-    stickers?.forEach( sticker => {
-        sticker.el.addEventListener("touchstart", (event) => {
-            console.log(event)
-            sticker.x = sticker.el.offsetLeft - event.clientX
-            sticker.y = sticker.el.offsetTop - event.clientY
-            sticker.isDragging = true
-        }, false)
-
-
-
-        // sticker.el?.addEventListener('touchstart', (event) => {
-        //     console.log(event)
-        //     sticker.x = sticker.el.offsetLeft - event.clientX
-        //     sticker.y = sticker.el.offsetTop - event.clientY
-        //     sticker.isDragging = true
-        // }, false)
-    })
-
-    document.addEventListener("touchmove", (event) => {
-
-        event.preventDefault();
-
-        const sticker = stickers?.filter(sticker => { return sticker.isDragging } )[0]
-
-        if (sticker) {
-            sticker.el.style.left = (event.clientX + sticker.x) + 'px';
-            sticker.el.style.top  = (event.clientY + sticker.y) + 'px';
-        }
-    },false)
-
-    document.addEventListener('touchend', function() {
-        stickers.forEach( (sticker) => {
-            sticker.isDragging = false
-        })
-    }, false);
-// })
+}

b0a2afd3ce6a4c63048d0713105152f719e1b5a5
isgleb <71262423+isgleb@users.noreply.github.com>
2023-04-17 23:06:40 +0300

    the rest added

--- newMain.js
+++ newMain.js
@@ -1,3 +1,22 @@
+
+const label = document.getElementsByClassName('label-wrapper')[0];
+label?.addEventListener('click', showAd);
+
+// async function showLabel(delayTime) {
+    new Promise(resolve => setTimeout(resolve, 400)).finally(() => {
+        label?.classList.remove("wrapper-hidden");
+    })
+// }
+
+function showAd() {
+    label.classList.add("wrapper-hidden")
+    label.addEventListener("transitionend", ()=>{
+        label.style.display="none"
+    });
+}
+
+// await showLabel(400)
+
 let activeBlock = null;
 let initialX = 0;
 let initialY = 0;
@@ -5,38 +24,18 @@ let initialY = 0;
 const blocks = document.querySelectorAll('.sticker');
 
 blocks.forEach(block => {
-    // block.addEventListener('mousedown', dragStart);
     block.addEventListener('touchstart', dragStart);
-    // block.addEventListener('mouseup', dragEnd);
     block.addEventListener('touchend', dragEnd);
-    // block.addEventListener('mousemove', drag);
     block.addEventListener('touchmove', drag);
 })
 
-// for (let i = 0; i < blocks.length; i++) {
-//     const block = blocks[i];
-//
-//     block.addEventListener('mousedown', dragStart);
-//     block.addEventListener('touchstart', dragStart);
-//     block.addEventListener('mouseup', dragEnd);
-//     block.addEventListener('touchend', dragEnd);
-//     block.addEventListener('mousemove', drag);
-//     block.addEventListener('touchmove', drag);
-// }
-
-
 function dragStart(e) {
-    // if (e.type === 'touchstart') {
-        initialX = e.touches[0].clientX - e.target.offsetLeft;
-        initialY = e.touches[0].clientY - e.target.offsetTop;
-    // } else {
-    //     initialX = e.clientX - e.target.offsetLeft;
-    //     initialY = e.clientY - e.target.offsetTop;
-    // }
+    initialX = e.touches[0].clientX - e.target.offsetLeft;
+    initialY = e.touches[0].clientY - e.target.offsetTop;
     activeBlock = e.target;
 }
 
-function dragEnd(e) {
+function dragEnd() {
     initialX = 0;
     initialY = 0;
     activeBlock = null;
@@ -45,19 +44,8 @@ function dragEnd(e) {
 function drag(e) {
     if (activeBlock) {
         e.preventDefault();
-        // let currentX;
-        // let currentY;
-
         let currentX = e.touches[0].clientX - initialX;
         let currentY = e.touches[0].clientY - initialY;
-
-        // if (e.type === 'touchmove') {
-        //     currentX = e.touches[0].clientX - initialX;
-        //     currentY = e.touches[0].clientY - initialY;
-        // } else {
-        //     currentX = e.clientX - initialX;
-        //     currentY = e.clientY - initialY;
-        // }
         e.target.style.left = currentX + "px";
         e.target.style.top = currentY + "px";
     }
